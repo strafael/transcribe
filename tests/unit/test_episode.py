8af87ed06bbc469e02497a09cf2d41271d53c2dd
Rafael Santos <rafaelpsantos@gmail.com>
2021-11-12 08:15:04 -0300

    Add test to broken segment case

--- tests/unit/test_episode.py
+++ tests/unit/test_episode.py
@@ -26,3 +26,29 @@ def test_get_segment_transcript(start_time, end_time, expected):
     episode = Episode.from_transcript_text("1", text)
     transcript = episode.get_segment_transcript(start_time, end_time)
     assert transcript == expected
+
+
+@pytest.mark.parametrize(
+    "start_time,end_time,expected",
+    [
+        (480, 482, "when the honeycrisp finally get to"),
+    ]
+)
+def test_get_broken_segment_transcript(start_time, end_time, expected):
+    """
+    Explanation of this test case.
+
+    The following is the text from 8:00 to 8:04, i.e. from 480 to 484 seconds.
+        when the honeycrisp finally get to the store, they do great
+
+    This means that 11 words are spread across 4 seconds. Hence, we should
+    return 5.5 words corresponding to the 2 seconds of transcript requested.
+    Since, half a word doesn't make sense, we will round up and return 6 words.
+
+    """
+    with open("tests/testdata/episode_1.txt") as fp:
+        text = fp.read()
+
+    episode = Episode.from_transcript_text("1", text)
+    transcript = episode.get_segment_transcript(start_time, end_time)
+    assert transcript == expected
